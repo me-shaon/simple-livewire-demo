e6adb3339274d5699385e6dc218d7e610be6dc07
Ahmed shamim <shaon.cse81@gmail.com>
2023-12-30 14:50:05 +0600

    feat: create command to update weather

---
+++ app/Console/Commands/ChangeWeather.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Weather;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\select;
+
+class ChangeWeather extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:change-weather';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Change weather with the given values';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $id = text(
+            label: 'Please provide the weather id:',
+            required: true
+        );
+
+        $temperature = text(
+            label: 'What is the temperature now?',
+            required: true
+        );
+
+        $condition = select(
+            'What is the weather condition?',
+            Weather::CONDITION_LIST,
+        );
+
+        $weather = Weather::findOrFail($id);
+        $weather->update([
+            'temperature' => $temperature,
+            'condition' => $condition
+        ]);
+
+    }
+}
