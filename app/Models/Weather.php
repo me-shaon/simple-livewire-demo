3739cff8ee0615d7b397aa55d45e658f500709a0
Ahmed shamim <shaon.cse81@gmail.com>
2023-12-30 08:40:30 +0600

    chore: migration, model, factory, seeder add

---
+++ app/Models/Weather.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Weather extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'city',
+        'temperature'
+    ];
+}
